7207df445f099700c72b6f7d5368ca89348ddc9e
BeLeap <changseo_jang@korea.ac.kr>
2021-09-10 00:42:38 +0900

    fix

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import json
 import os
 from datetime import datetime, timedelta, timezone
 from typing import Union
+from discord_slash.model import SlashCommandOptionType
 from discord_slash.utils.manage_commands import create_option
 
 from dotenv import load_dotenv
 from selenium import webdriver
 
 import discord
-from discord import Client, Embed, Intents
+from discord import Client, Intents, Embed
 from discord_slash import SlashCommand, SlashContext
-from discord_slash.model import SlashCommandOptionType
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
@@ -21,19 +21,19 @@ ARCH = os.getenv('ARCH')
 webdriver_options = webdriver.ChromeOptions()
 if ARCH.upper().startswith('LIN'):
     webdriver_options.add_argument('--no-sandbox')
-webdriver_options.add_argument('headless')
+# webdriver_options.add_argument('headless')
 executable_path = './chromedriver.exe' if ARCH.upper(
 ).startswith('WIN') else './chromedriver'
 
 client = Client(intents=Intents.all())
 slash = SlashCommand(client, sync_commands=True)
+driver = webdriver.Chrome(executable_path, options=webdriver_options)
 
 
-def upcoming_ctf_list(count):
+async def upcoming_ctf_list(count):
     if int(count) < 1:
         raise Exception
 
-    driver = webdriver.Chrome(executable_path, options=webdriver_options)
     driver.get(f'https://ctftime.org/api/v1/events/?limit={count}')
     response = driver.find_element_by_css_selector('body').text
     response_dict = json.loads(response)
@@ -43,15 +43,13 @@ def upcoming_ctf_list(count):
         row_parsed = {'title': row['title'], 'description': row['description'],
                       'start': datetime.fromisoformat(row['start']).astimezone(timezone(timedelta(hours=9))), 'finish': datetime.fromisoformat(row['finish']).astimezone(timezone(timedelta(hours=9))), 'logo': row['logo'], 'url': row['url'], 'weight': row['weight']}
         data.append(row_parsed)
-
-    driver.close()
     return data
 
 
-def search_upcoming_ctf(count, keyword):
+async def search_upcoming_ctf(count, keyword):
     if count > 100:
         count = 100
-    data = upcoming_ctf_list(count)
+    data = await upcoming_ctf_list(count)
 
     result = []
     for datum in data:
@@ -71,7 +69,7 @@ def search_upcoming_ctf(count, keyword):
 ])
 async def upcoming(ctx: SlashContext, **kwargs):
     count = kwargs.get('max_count', 1)
-    data = upcoming_ctf_list(count)
+    data = await upcoming_ctf_list(count)
 
     for datum in data:
         if datum:
@@ -112,7 +110,7 @@ async def search(ctx: SlashContext, **kwargs):
     keyword = kwargs.get('keyword', '')
     count = kwargs.get('count', 10)
 
-    data = search_upcoming_ctf(count, keyword)
+    data = await search_upcoming_ctf(count, keyword)
 
     if data:
         for datum in data:
