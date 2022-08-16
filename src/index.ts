095e4c59442af33a547996bd8349578e878a83eb
BeLeap <beleap@beleap.codes>
2022-08-16 13:53:49 +0900

    feat:configure dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+import dotenv from 'dotenv';
+import path from 'path';
+
+if (process.env.NODE_ENV === 'dev') {
+  dotenv.config({
+    path: path.resolve(process.cwd(), '.env.dev'),
+  });
+}
+
+const TOKEN = process.env.TOKEN ?? "";
